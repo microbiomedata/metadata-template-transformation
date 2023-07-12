d9b307cb71e44efd3381eadb1aeef1f337386f94
Sujay Patil <sujaysanjeev.patil@gmail.com>
2023-07-12 13:23:56 -0700

    refactor based on OOP pattern

--- etl.py
+++ etl.py
@@ -1,122 +1,178 @@
-import pandas as pd
 import os
 import json
+
+import pandas as pd
 import click
-import dotenv
 import requests
 
-
-user_facility_dict = {
-    "emsl": "emsl_data",
-    "jgi_mg": "jgi_mg_data",
-    "jgi_mt": "jgi_mt_data",
-}
+from typing import Dict, Any, List, Union, Optional
+from dotenv import load_dotenv
 
 
-def load_dotenv():
-    env_path = dotenv.find_dotenv()
-    if len(env_path) > 0:
-        dotenv.load_dotenv(dotenv_path=env_path, override=True)
+class MetadataRetriever:
+    """
+    Retrieves metadata records from a given submission ID and user facility.
+    """
+
+    USER_FACILITY_DICT: Dict[str, str] = {
+        "emsl": "emsl_data",
+        "jgi_mg": "jgi_mg_data",
+        "jgi_mt": "jgi_mt_data",
+    }
+
+    def __init__(self, metadata_submission_id: str, user_facility: str) -> None:
+        """
+        Initialize the MetadataRetriever.
+
+        :param metadata_submission_id: The ID of the metadata submission.
+        :param user_facility: The user facility to retrieve data from.
+        """
+        self.metadata_submission_id = metadata_submission_id
+        self.user_facility = user_facility
+        self.env: Dict[str, str] = dict(os.environ)
+
+    def retrieve_metadata_records(self) -> pd.DataFrame:
+        """
+        Retrieves the metadata records for the given submission ID and user facility.
+
+        :return: The retrieved metadata records as a Pandas DataFrame.
+        """
+        response: Dict[str, Any] = requests.get(
+            f"https://data.microbiomedata.org/api/metadata_submission/{self.metadata_submission_id}",
+            cookies={"session": self.env["DATA_PORTAL_COOKIE"]},
+        ).json()
+
+        if "soil_data" in response["metadata_submission"]["sampleData"]:
+            soil_data: List[Dict[str, Any]] = response["metadata_submission"][
+                "sampleData"
+            ]["soil_data"]
+            soil_data_df: pd.DataFrame = pd.DataFrame(soil_data)
+        else:
+            soil_data_df = pd.DataFrame()
+
+        common_df: pd.DataFrame = pd.DataFrame()
+        if self.user_facility in self.USER_FACILITY_DICT:
+            user_facility_data: Dict[str, Any] = response["metadata_submission"][
+                "sampleData"
+            ].get(self.USER_FACILITY_DICT[self.user_facility], {})
+            common_df = pd.DataFrame(user_facility_data)
+
+        if soil_data_df.empty and not common_df.empty:
+            df = common_df
+        elif common_df.empty and not soil_data_df.empty:
+            df = soil_data_df
+        elif not common_df.empty and not soil_data_df.empty:
+            common_cols: List[str] = list(
+                set(common_df.columns.to_list()) & set(soil_data_df.columns.to_list())
+            )
+            if "source_mat_id" in common_cols:
+                common_cols.remove("source_mat_id")
+
+            common_df = common_df.drop(columns=common_cols)
+            df = pd.merge(soil_data_df, common_df, on="source_mat_id")
+        else:
+            raise ValueError(
+                f"The submission metadata record: {self.metadata_submission_id} is empty."
+            )
+
+        return df
 
 
-def retrieve_metadata_records(
-    user_facility, metadata_submission_id: str
-) -> pd.DataFrame:
-    env = dict(os.environ)
-    response = requests.request(
-        "GET",
-        f"https://data.microbiomedata.org/api/metadata_submission/{metadata_submission_id}",
-        cookies={"session": env["DATA_PORTAL_COOKIE"]},
-    ).json()
+class SpreadsheetCreator:
+    """
+    Creates a spreadsheet based on a JSON mapper and metadata DataFrame.
+    """
 
-    df = pd.DataFrame()
-    if "soil_data" in response["metadata_submission"]["sampleData"]:
-        soil_data = response["metadata_submission"]["sampleData"]["soil_data"]
-        soil_data_df = pd.DataFrame(soil_data)
+    def __init__(
+        self,
+        json_mapper: Dict[str, Dict[str, Union[str, List[str]]]],
+        metadata_df: pd.DataFrame,
+    ) -> None:
+        """
+        Initialize the SpreadsheetCreator.
 
-    common_df = pd.DataFrame()
-    if user_facility in user_facility_dict:
-        if (
-            user_facility_dict[user_facility]
-            in response["metadata_submission"]["sampleData"]
-        ):
-            common_data = response["metadata_submission"]["sampleData"][
-                user_facility_dict[user_facility]
+        :param json_mapper: The JSON mapper specifying column mappings.
+        :param metadata_df: The metadata DataFrame to create the spreadsheet from.
+        """
+        self.json_mapper = json_mapper
+        self.metadata_df = metadata_df
+
+    def combine_headers_df(self, header: bool) -> pd.DataFrame:
+        """
+        Combines and formats the headers DataFrame.
+
+        :param header: True if the headers should be included, False otherwise.
+        :return: The combined headers DataFrame.
+        """
+        d: Dict[str, List[Union[str, List[str]]]] = {}
+        for k, v in self.json_mapper.items():
+            l: List[Union[str, List[str]]] = [
+                h for h_n, h in v.items() if h_n != "sub_port_mapping"
             ]
-            common_df = pd.DataFrame(common_data)
+            d[k] = l
 
-    if soil_data_df.empty and not common_df.empty:
-        df = common_df
-    elif common_df.empty and not soil_data_df.empty:
-        df = soil_data_df
-    elif not common_df.empty and not soil_data_df.empty:
-        common_cols = list(
-            set(common_df.columns.to_list()) & set(soil_data_df.columns.to_list())
-        )
-        if "source_mat_id" in common_cols:
-            common_cols.remove("source_mat_id")
+        headers_df: pd.DataFrame = pd.DataFrame(d)
 
-        common_df = common_df.drop(columns=common_cols)
-        df = pd.merge(soil_data_df, common_df, on="source_mat_id")
-    else:
-        raise ValueError(
-            f"The submission metadata record: {metadata_submission_id} is empty."
-        )
+        if header:
+            last_row = headers_df.iloc[-1]
+            column_values: List[str] = list(last_row)
 
-    return df
+            headers_df = headers_df.drop(headers_df.index[-1])
+            headers_df.loc[len(headers_df)] = headers_df.columns.to_list()
+            headers_df.columns = column_values
 
-
-def combine_headers_df(json_mapper: dict, header: bool) -> pd.DataFrame:
-    d = {}
-    for k, v in json_mapper.items():
-        l = list()
-        for h_n, h in v.items():
-            if h_n != "sub_port_mapping":
-                l.append(h)
-        d[k] = l
-    headers_df = pd.DataFrame(d)
-
-    if header:
-        last_row = headers_df.iloc[-1]
-        column_values = list(last_row)
-
-        headers_df = headers_df.drop(headers_df.index[-1])
-        headers_df.loc[len(headers_df)] = headers_df.columns.to_list()
-        headers_df.columns = column_values
-
-        shift = 1
-        headers_df = pd.concat(
-            [headers_df.iloc[-shift:], headers_df.iloc[:-shift]], ignore_index=True
-        )
+            shift = 1
+            headers_df = pd.concat(
+                [headers_df.iloc[-shift:], headers_df.iloc[:-shift]], ignore_index=True
+            )
 
         return headers_df
 
-    return headers_df
+    def combine_sample_rows_df(self) -> pd.DataFrame:
+        """
+        Combines and formats the sample rows DataFrame.
+
+        :return: The combined sample rows DataFrame.
+        """
+        rows_df: pd.DataFrame = pd.DataFrame()
+        for k, v in self.json_mapper.items():
+            if (
+                "sub_port_mapping" in v
+                and v["sub_port_mapping"] in self.metadata_df.columns.to_list()
+            ):
+                if "header" in v:
+                    rows_df[v["header"]] = self.metadata_df[v["sub_port_mapping"]]
+                else:
+                    rows_df[k] = self.metadata_df[v["sub_port_mapping"]]
+
+        return rows_df
+
+    def combine_headers_and_rows(
+        self, headers_df: pd.DataFrame, rows_df: pd.DataFrame
+    ) -> pd.DataFrame:
+        """
+        Combines the headers and sample rows DataFrames.
+
+        :param headers_df: The headers DataFrame.
+        :param rows_df: The sample rows DataFrame.
+        :return: The combined DataFrame.
+        """
+        return pd.concat([headers_df, rows_df])
+
+    def create_spreadsheet(self, header: bool) -> pd.DataFrame:
+        """
+        Creates the spreadsheet based on the JSON mapper and metadata DataFrame.
+
+        :param header: True if the headers should be included, False otherwise.
+        :return: The created spreadsheet.
+        """
+        headers_df = self.combine_headers_df(header)
+        rows_df = self.combine_sample_rows_df()
+        spreadsheet = self.combine_headers_and_rows(headers_df, rows_df)
+        return spreadsheet
 
 
-def combine_sample_rows_df(
-    submission_df: pd.DataFrame, json_mapper: dict
-) -> pd.DataFrame:
-    rows_df = pd.DataFrame()
-    for k, v in json_mapper.items():
-        if (
-            "sub_port_mapping" in v
-            and v["sub_port_mapping"] in submission_df.columns.to_list()
-        ):
-            if "header" in v:
-                rows_df[v["header"]] = submission_df[v["sub_port_mapping"]]
-            else:
-                rows_df[k] = submission_df[v["sub_port_mapping"]]
-
-    return rows_df
-
-
-def combine_headers_and_rows(
-    headers_df: pd.DataFrame, rows_df: pd.DataFrame
-) -> pd.DataFrame:
-    return pd.concat([headers_df, rows_df])
-
-
+@click.command()
 @click.option("--submission", "-s", required=True, help="Metadata submission id.")
 @click.option(
     "--user-facility", "-u", required=True, help="User facility to send data to."
@@ -135,17 +191,27 @@ def combine_headers_and_rows(
     required=True,
     help="Path to result output XLSX file.",
 )
-@click.command()
-def cli(submission, user_facility, header, mapper, output):
+def cli(
+    submission: str, user_facility: str, header: bool, mapper: str, output: str
+) -> None:
+    """
+    Command-line interface for creating a spreadsheet based on metadata records.
+
+    :param submission: The ID of the metadata submission.
+    :param user_facility: The user facility to retrieve data from.
+    :param header: True if the headers should be included, False otherwise.
+    :param mapper: Path to the JSON mapper specifying column mappings.
+    :param output: Path to the output XLSX file.
+    """
     load_dotenv()
-    metadata_submission = retrieve_metadata_records(user_facility, submission)
+    metadata_retriever = MetadataRetriever(submission, user_facility)
+    metadata_df = metadata_retriever.retrieve_metadata_records()
 
     with open(mapper, "r") as f:
-        json_mapper = json.load(f)
+        json_mapper: Dict[str, Dict[str, Union[str, List[str]]]] = json.load(f)
 
-    headers_df = combine_headers_df(json_mapper, header)
-    rows_df = combine_sample_rows_df(metadata_submission, json_mapper)
-    user_facility_spreadsheet = combine_headers_and_rows(headers_df, rows_df)
+    spreadsheet_creator = SpreadsheetCreator(json_mapper, metadata_df)
+    user_facility_spreadsheet = spreadsheet_creator.create_spreadsheet(header)
     user_facility_spreadsheet.to_excel(output, index=False)
 
 
